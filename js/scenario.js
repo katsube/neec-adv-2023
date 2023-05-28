f3560aba73d866743b9eeae2fcbed09149a3b9f7
Makito Katsube <katsubemakito@gmail.com>
2023-05-28 15:44:43 +0900

    day2の内容を作成

---
+++ js/scenario.js
@@ -0,0 +1,10 @@
+const INDEX_COMMAND = 0;
+const INDEX_VALUE   = 1;
+
+const SCENARIO = [
+  ['SERIF',     '吾輩は猫である'],
+  ['BGM-PLAY',  'bgm_peaceful.mp3'],
+  ['BG-IMAGE',  'beach_001.jpg'],
+  ['SERIF',     '名前はまだ無い'],
+  ['SERIF',     'どこで生まれたかとんと見当がつかぬ']
+];
