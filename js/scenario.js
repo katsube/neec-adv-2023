73588b8dd4c7231e8862678da3616356bf71d30e
Makito Katsube <katsubemakito@gmail.com>
2023-06-05 15:30:57 +0900

    2023-06-05授業分

--- js/scenario.js
+++ js/scenario.js
@@ -1,7 +1,7 @@
 const scenario = [
   ['SERIF', '吾輩は猫である'],
   ['BGM',   'bgm_peaceful.mp3'],
-  ['SERIF', '名前はまだ無い'],
+  ['SERIF', '名前は%name%だ'],
   ['BG',    'beach_001.jpg'],
   ['SERIF', 'どこで生まれたかとんと見当がつかぬ']
 ];
