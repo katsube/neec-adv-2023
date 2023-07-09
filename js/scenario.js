a52584cdd0133a4fbc74725e401b592fdce808e0
Makito Katsube <katsubemakito@gmail.com>
2023-07-09 19:24:45 +0900

    簡易的なskip機能を実装

--- js/scenario.js
+++ js/scenario.js
@@ -10,9 +10,9 @@ const scenario = [
 
 let scenario = [
   ['SERIF', '吾輩は猫である。名前はまだ無い。', 'VC01_00001.mp3'],
-  ['BGM',   'bgm_peaceful.mp3'],
-  ['CHARA', [2, '9.png']],
-  ['CHARA', [3, '10.png']],
+  ['BGM',   'bgm_peaceful.mp3', 'skip'],
+  ['CHARA', [2, '9.png'], 'skip'],
+  ['CHARA', [3, '10.png'], 'skip'],
   ['SERIF', 'どこで生れたかとんと見当がつかぬ。', 'VC01_00002.mp3'],
   ['SERIF', '何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。', 'VC01_00003.mp3'],
   ['SERIF', '吾輩はここで始めて人間というものを見た。', 'VC01_00004.mp3'],
@@ -20,7 +20,7 @@ let scenario = [
   ['SERIF', 'この書生というのは時々我々を捕つかまえて煮て食うという話である。',  'VC01_00006.mp3'],
   ['SERIF', 'しかし実際にそんな事をするときがあるかというと今更ながらに疑問である。', 'VC01_00007.mp3'],
   ['SERIF', 'ただ彼の掌に載せられてスーと持ち上げられた時何だかフワフワした感じがあったばかりである。', 'VC01_00008.mp3'],
-  ['CHARA', [2, 'blank.png']],
-  ['CHARA', [3, 'blank.png']],
+  ['CHARA', [2, 'blank.png'], 'skip'],
+  ['CHARA', [3, 'blank.png'], 'skip'],
   ['NEXT',  'part2']
 ];
